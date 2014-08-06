089ca9fbb6a6317ad5e96e6c5525364ebfa68808
Selina Musuta <selina@codeforprogress.org>
2014-08-06 13:19:07 -0400

    added a config file

--- sunlightBill_lookup/open_states/urls.py
+++ sunlightBill_lookup/open_states/urls.py
@@ -4,6 +4,8 @@ from open_states import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^widget_list/(?P<bill_id>.*?)/(?P<state>.*?)/', views.get_widget_list, name='widget_list_by_state_and_bill')
+    url(r'^(?P<bill_id>.*?)/(?P<state>.*?)/$', views.get_widget_list, name='widget_list_by_state_and_bill')
      #url(r'^widget_list/$', views.get_widget_list, name='widget list')
 )
+
+#widget_list/
--- sunlightBill_lookup/open_states/views.py
+++ sunlightBill_lookup/open_states/views.py
@@ -2,8 +2,8 @@ from django.template import Context
 from django.shortcuts import render 
 import requests
 from forms import SearchBillForm, WidgetListForm
-from django.http import HttpResponseRedirect
-
+from django.http import HttpResponseRedirect, HttpResponse
+import config 
 
 def index(request):
 	#Create a variable form for the SearchBillForm function in forms.py 
@@ -28,7 +28,7 @@ def index(request):
 # This is a stand alone function that will pull the input field data/parameters: Api_key, bill_id, open_states_url.  
 def get_openstates_api(bill_id, state):
 	#Create a parameters variable from the openstates api rules including apikey, bill_id, and state
-	params = {'apikey': 'please put your api key here', 'bill_id': bill_id, 'state': state} 
+	params = {'apikey': api_key, 'bill_id': bill_id, 'state': state} 
 	s = requests.get("http://openstates.org/api/v1/bills/", params=params)
 	json_response = s.json()
 	return json_response
