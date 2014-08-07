135c4c38cab0568b265c8ddf3a6be58625add8cf
aliyarahman <aliya.rahman@yahoo.com>
2014-08-07 14:35:10 -0400

    Added content to third frame

--- sunlightBill_lookup/open_states/views.py
+++ sunlightBill_lookup/open_states/views.py
@@ -35,15 +35,21 @@ def get_openstates_api(bill_id, state):
 # We need request, bill_id, and state in the input section on the next line in order to pass variables to the API call
 def get_widget_list(request, bill_id, state):
 	#Create a variable form for the SearchBillForm function in forms.py 
-	form = WidgetListForm()
+	form = SearchBillForm()
+	if request.method == 'POST': 
+		# Create a form variable that will receive the SearchBillForm post request
+		form = SearchBillForm(request.POST)
+		#If form is valid.  All validation rules are listed in forms.py 
+        if form.is_valid(): 
+         # Process the data in form.cleaned_data by creating a bill_id and state variable
+         bill_id = form.cleaned_data['bill_number']
+         state = form.cleaned_data['state']
+         #redirect to the widget view
+         return HttpResponseRedirect('/open_states/get_widget_list/%s/%s/' % (bill_id, state)) # Here we have to add the /open_states prefix to get Django to find the right urls.py files
 	# If the form has been submitted in the SearchBIllForm using the Post request method  
 	if request.method == 'GET':
 		#json_response variable with get_openstates_api function
 		json_response = get_openstates_api(bill_id, state)
 		# Create a form variable that will receive the WidgetListForm get request
 
-		results = []
-		for bill in json_response:
-			results.append(bill['title'])
-
-		return render(request, 'open_states/widget_list.html',{'results': results})
+		return render(request, 'open_states/widget_list.html',{'json_response': json_response, 'form':form})
