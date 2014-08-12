6c56da6768cf3ce9a680549a425366ca95ab2565
Selina Musuta <selina@codeforprogress.org>
2014-08-12 12:28:08 -0400

    add javascript hide functionality and fixed footer margins

--- sunlightBill_lookup/open_states/static/open_states/javascript.js
+++
@@ -1,7 +0,0 @@
-
-$('.single_bill_holder').click(function(){
-$(this).fadeOut();
-});
-$('h1.top_headline').click(function(){
-$('div').fadeIn();
-});
---
+++ sunlightBill_lookup/open_states/static/open_states/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() { 
+
+	$('button').click(function(){
+		$(this).hide();
+	});
+
+$('h1.top_headline').click(function(){
+$('div').fadeIn();
+});
