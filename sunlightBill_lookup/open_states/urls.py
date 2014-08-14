4d8e9d080b3ecb8c0c78b72a50fcc0450b2eb5d6
Selina Musuta <selina@codeforprogress.org>
2014-08-13 21:51:07 -0400

    iframe within my widget list template

--- sunlightBill_lookup/open_states/urls.py
+++ sunlightBill_lookup/open_states/urls.py
@@ -5,7 +5,7 @@ from open_states import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^get_widget_list/(?P<sunlightAPIs>.+?)/(?P<bill_id>.+?)/(?P<state>\S+?)/$', views.get_widget_list, name='get_widget list'),
-    url(r'^get_widget_list/(?P<sunlightAPIs>.+?)/(?P<bill_id>.+?)/$', views.get_widget_list, name='get_widget_congress list'),
+    url(r'^preview/(?P<sunlightAPIs>.+?)/(?P<bill_id>.+?)/(?P<state>\S+?)/$', views.preview, name='preview'),
 )
 
 # Note that in the get_widget_list url, we need to give it a place to accept both the bill_id as a number and the state ID as a string
--- sunlightBill_lookup/open_states/views.py
+++ sunlightBill_lookup/open_states/views.py
@@ -5,26 +5,46 @@ from forms import SearchBillForm, WidgetListForm
 from django.http import HttpResponseRedirect, HttpResponse
 #import config 
 
+
+#Create a preview function that will render the iframe into the landing page(index)
+def preview(request, sunlightAPIs, bill_id, state):
+	form = SearchBillForm(request.POST)
+
+	if request.method == 'GET':
+		if sunlightAPIs == "1":
+			json_response = get_opencongress_api(bill_id)
+			congress_bills=[]
+			state_bills=[]
+			congress_bills.append(json_response['results'])
+			congress_bills = congress_bills[0]
+			request.session['congress_bills'] = congress_bills
+		else:
+			json_response = get_openstates_api(bill_id, state)
+			state_bills=[]
+			state_bills.append(json_response)
+	return render(request, 'open_states/just_preview.html',{'state_bills': state_bills, 'form':form, 'congress_bills':congress_bills})
+
 def index(request):
 	#Create a variable form for the SearchBillForm function in forms.py 
 	form = SearchBillForm()
 	# If the form has been submitted in the SearchBIllForm using the Post request method  
-	if request.method == 'POST': 
+	if request.method == 'POST':
 		# Create a form variable that will receive the SearchBillForm post request
 		form = SearchBillForm(request.POST)
 		#If form is valid.  All validation rules are listed in forms.py 
-        if form.is_valid(): 
+        if form.is_valid():
+        	# Process the data in form.cleaned_data by creating a bill_id and state variable
         	sunlightAPIs = form.cleaned_data['sunlightAPIs']
         	bill_id = form.cleaned_data['bill_number']
          	state = form.cleaned_data['state']
+         	request.session['state_bills'] = []
+         	request.session['congress_bills'] = []
          	if state=="":
          		state="none"
-
-         	# Process the data in form.cleaned_data by creating a bill_id and state variable
-         	#redirect to the widget view
+         	#Redirect to the get_widget_list url that includes strings of the sunlightAPIs, bill_id, and state
+         	# Here we have to add the /open_states prefix to get Django to find the right urls.py files
          	return HttpResponseRedirect('/open_states/get_widget_list/%s/%s/%s/' % (sunlightAPIs, bill_id, state)) # Here we have to add the /open_states prefix to get Django to find the right urls.py files
-	return render(request, 'open_states/index.html', {'form': form})
-
+	return render(request, 'open_states/index.html', {'form': form}) 
 
 # This is a stand alone function that will pull the input field data/parameters: Api_key, bill_id, open_states_url. And then return a json_response from the Open Congress API. 
 def get_openstates_api(bill_id, state):
@@ -46,34 +66,30 @@ def get_opencongress_api(bill_id):
 	return json_response
 
 #This function will pass the state, bill_id, and request 
-# We need request, bill_id, and state in the input section on the next line in order to pass variables to the API call
+# Pass request, sunlightAPIs, bill_id, and state in the input section on the next line in order to pass variables to the API call
 def get_widget_list(request, sunlightAPIs, bill_id, state):
+	#session variables with Aliya
+	state_bills = request.session['state_bills']
+	congress_bills = request.session['congress_bills']
 	form = SearchBillForm()
-	if request.method == 'POST': 
+	iframe_url="http://127.0.0.1:8000/open_states/preview/"+str(sunlightAPIs)+"/"+bill_id+"/"+state+"/"
+	if request.method == 'POST':
 		form = SearchBillForm(request.POST)
-        if form.is_valid(): 
-        	sunlightAPIs = form.cleaned_data['sunlightAPIs']
+		if form.is_valid():
+			sunlightAPIs = form.cleaned_data['sunlightAPIs']
         	bill_id = form.cleaned_data['bill_number']
          	state = form.cleaned_data['state']
          	if state=="":
   				state="none"
-         	return HttpResponseRedirect('/open_states/get_widget_list/%s/%s/%s/' % (sunlightAPIs, bill_id, state)) # Here we have to add the /open_states prefix to get Django to find the right urls.py files 
+  			# Here we have to add the /open_states prefix to get Django to find the right urls.py files 
+         	return HttpResponseRedirect('/open_states/get_widget_list/%s/%s/%s/' % (sunlightAPIs, bill_id, state)) 
 	if request.method == 'GET':
 		if sunlightAPIs == "1":
 			json_response = get_opencongress_api(bill_id)
-			congress_bills = json_response['results']
-			state_bills = []
-#			try:
-#				congress_bills = congress_bills.append(json_response['results'])
-#			except:
-#				congress_bills = []
+			congress_bills.append(json_response['results'])
+			congress_bills = congress_bills[0]
+			request.session['congress_bills'] = congress_bills
 		else:
 			json_response = get_openstates_api(bill_id, state)
-			state_bills = json_response
-			congress_bills = []
-#			try:
-#				state_bills = state_bills.append(json_response)
-#			except:
-#				state_bills = []
-			# Get the slice of bills for the state json object state_bills = json_response[????]
-		return render(request, 'open_states/widget_list.html',{'state_bills': state_bills, 'form':form, 'congress_bills':congress_bills})
+			state_bills.append(json_response)
+		return render(request, 'open_states/widget_list.html',{'state_bills': state_bills, 'form':form, 'congress_bills':congress_bills, 'iframe_url':iframe_url})
